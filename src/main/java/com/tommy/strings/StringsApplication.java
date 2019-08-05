ee5f64532081dc4f65d4dd0c4d979c02b362664d
Tommy Seo <tommyseo7m@gmail.com>
2019-08-05 13:26:29 -0700

    first commit

---
+++ src/main/java/com/tommy/strings/StringsApplication.java
@@ -0,0 +1,29 @@
+package com.tommy.strings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+@RestController
+public class StringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringsApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String helloPage() {
+		return "Hello client! How are you doing?";
+	}
+	
+	@RequestMapping("/random")
+	public String randomPage() {
+		return "Spring Boot is great! So easy to just respond with strings";
+	}
+	
+
+}
